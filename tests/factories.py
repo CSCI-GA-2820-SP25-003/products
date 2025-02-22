78b18110ebb898f0e6aa75bf4a0d28375b7c0701
ChentaoMa <cm6811@nyu.edu>
2025-02-22 17:51:04 +0000

    Set up the schema and created basic model tests

--- tests/factories.py
+++ tests/factories.py
@@ -2,19 +2,28 @@
 Test Factory to make fake objects for testing
 """
 
+from datetime import date
 import factory
-from service.models import YourResourceModel
+from service.models import Product
+from factory.fuzzy import FuzzyChoice, FuzzyText, FuzzyDecimal, FuzzyDate
 
 
-class YourResourceModelFactory(factory.Factory):
+class ProductFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Product
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    name = FuzzyChoice(
+        choices=["E-Reader", "Vacuum Cleaner", "Jeans", "Potato Chips", "Calculator"]
+    )
 
     # Todo: Add your other attributes here...
+
+    description = FuzzyText(length=256)
+    price = FuzzyDecimal(10.00, 500.00, precision=2)
+    created_time = FuzzyDate(date(2008, 1, 1))
+    updated_time = FuzzyDate(date(2008, 1, 1))

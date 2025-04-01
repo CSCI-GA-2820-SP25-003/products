532f76408a3718278b01f8fb83668cd2e1ce239b
Claire Liu <tl4151@nyu.edu>
2025-04-01 03:12:54 +0000

    Remove trailing whitespace in config

--- service/config.py
+++ service/config.py
@@ -6,7 +6,7 @@ import logging
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
-    "DATABASE_URI", 
+    "DATABASE_URI",
     "postgresql+psycopg://postgres:postgres@localhost:5432/products"
 )
 

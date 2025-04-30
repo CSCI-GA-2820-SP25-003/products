8f7784014db16fdaea79103eb0097fa383960d56
Claire Liu <tl4151@nyu.edu>
2025-04-30 01:26:34 +0000

    Modify initialized data format and behave wait time

--- tests/factories.py
+++ tests/factories.py
@@ -22,7 +22,21 @@ class ProductFactory(factory.Factory):
         choices=["E-Reader", "Vacuum Cleaner", "Jeans", "Potato Chips", "Calculator"]
     )
 
-    description = FuzzyText(length=256)
-    image_url = FuzzyChoice(choices=["https://www.google.com", "https://www.nyu.edu"])
+    description = FuzzyChoice(
+        choices=[
+            "Product 1 description",
+            "Product 2 description",
+            "Product 3 description",
+            "Product 4 description",
+            "Product 5 description",
+        ]
+    )
+    image_url = FuzzyChoice(
+        choices=[
+            "https://t4.ftcdn.net/jpg/01/36/70/67/360_F_136706734_KWhNBhLvY5XTlZVocpxFQK1FfKNOYbMj.jpg",
+            "https://www.ikea.com/us/en/images/products/blahaj-soft-toy-shark__0710175_pe727378_s5.jpg",
+            "https://i.ebayimg.com/images/g/NIkAAOSwXSVhZDSc/s-l1200.jpg",
+        ]
+    )
     likes = 0
     price = FuzzyDecimal(10.00, 500.00, precision=2)
